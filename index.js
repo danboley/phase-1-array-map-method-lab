b398a876a14f6f5db85dc505b9cdfceb945c8b48
Daniel Boley <danboley1@gmail.com>
2022-08-31 16:37:45 -0400

    commit tree 8d72f50b6f8a16880ba59c538c1bfd7fa9cbb5d1

--- index.js
+++ index.js
@@ -11,6 +11,43 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//const titleCased = () => {
+//  return tutorials
+//}
+
+
+//function capitalizeFirstLetter() {
+//  return string.toUpperCase.charAt(0)
+//}
+
+//tutorials.forEach(() => {
+//  return capitalizeFirstLetter()
+//})
+
+//const titleCased = map(tutorials, function()) {
+//  return element.toUpperCase();
+//}
+
+// Time to Google... charAt() needed?
+
+//const titleCased = () => {
+//  return tutorials.map (element => {
+//  return element.toUpperCase().charAt(0);
+//  })
+//}
+
+//const titleCased = () => {
+//  return tutorials.map (element => {
+//    return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+//  })
+//}
+
 const titleCased = () => {
-  return tutorials
+  const newTutorials = tutorials.map((callback) => {
+    const casedTutorials = callback.split(' ').map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+    return casedTutorials
+  })
+return newTutorials
 }
